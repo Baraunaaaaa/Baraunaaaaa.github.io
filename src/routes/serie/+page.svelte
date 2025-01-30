9a76bca4a9b0b82a35d1b290c40e50fd67620dbf
Baraunaaaaa <manoelabarauna5@gmail.com>
2025-01-30 11:22:30 -0400

    minha

--- src/routes/serie/+page.svelte
+++ src/routes/serie/+page.svelte
@@ -1,17 +1,12 @@
+<a href="/minhaop"</a>
+
+
+
+
+
 <script>
   import { writable } from 'data.js';
   let activeTab = writable(0);
-
-  const series = [
-    { name: "The Bear", imdb: "https://www.imdb.com/title/tt14452776/" },
-    { name: "The Office", imdb: "https://www.imdb.com/title/tt0386676/" },
-    { name: "Brooklyn Nine-Nine", imdb: "https://www.imdb.com/title/tt2467372/" },
-    { name: "Black Bird", imdb: "https://www.imdb.com/title/tt4301160/" },
-    { name: "Stranger Things", imdb: "https://www.imdb.com/title/tt4574334/" },
-    { name: "Dexter", imdb: "https://www.imdb.com/title/tt0773262/" },
-    { name: "Sex and the City", imdb: "https://www.imdb.com/title/tt0159206/" }
-  ];
-
   let opinions = writable("");
   let publicOpinions = writable([]);
   let myOpinion = writable("");
